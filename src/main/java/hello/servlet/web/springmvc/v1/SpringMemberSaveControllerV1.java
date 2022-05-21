aaea2a3e0827bf2c4e3082d6e8fca712c41db784
Yunbok <qodbsqhr@naver.com>
2022-05-21 22:18:19 +0900

    프로젝트 마무리

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 @Controller
 public class SpringMemberSaveControllerV1 {
